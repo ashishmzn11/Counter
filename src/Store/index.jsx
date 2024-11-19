43aa3e2c91c6cad7c21d847bd24b375fddf63bb8
ashishmzn11 <ashishmzn11@gmail.com>
2024-11-19 12:25:02 +0530

    second message

--- src/Store/index.jsx
+++ src/Store/index.jsx
@@ -1,17 +1,17 @@
 import {createStore} from "redux";
 const INITIAL_VALUE={counter:0}
 const counterReducer=(store=INITIAL_VALUE,action)=>{
-  if(action.type==='INCREMENT'){
-return {counter: store.counter +1};
-  }
-  else  if(action.type==='DECREMENT'){
-    return { counter: store.counter -1};
-  }
-  else  if(action.type==='ADD'){
+  if(action.type==='ADD'){
     return { counter: store.counter + Number(action.payload.num)};
   }
   else  if(action.type==='SUB'){
     return { counter: store.counter - Number(action.payload.num)};
+  }
+  else  if(action.type==='MUL'){
+    return { counter: store.counter * Number(action.payload.num)};
+  }
+  else  if(action.type==='DIV'){
+    return { counter: store.counter / Number(action.payload.num)};
   }
   return store;
 }
--- src/component/Controler.jsx
+++ src/component/Controler.jsx
@@ -1,33 +1,50 @@
 import { useRef } from "react";
 import { useDispatch } from "react-redux";
 
-
-const Controler=()=>{
-  const dispatch=useDispatch();
-  const addelement=useRef();
-  const handleIncrement=()=>{
-    dispatch({type:"INCREMENT"});
-  }
-  const handledecrement=()=>{
-    dispatch({type:"DECREMENT"});
-  }
-  const handledAdd=()=>{
-    dispatch({type:"ADD", payload:{num:addelement.current.value,}});
-    addelement.current.value="";
-  }
-  const handledSub=()=>{
-    dispatch({type:"SUB", payload:{num:addelement.current.value,}});
-    addelement.current.value="";
-  }
-  return(
-  
+const Controler = () => {
+  const dispatch = useDispatch();
+  const addelement = useRef();
+  const handleMul = () => {
+    dispatch({ type: "MUL", payload: { num: addelement.current.value } });
+    addelement.current.value = "";
+  };
+  const handleDiv = () => {
+    dispatch({ type: "DIV", payload: { num: addelement.current.value } });
+    addelement.current.value = "";
+  };
+  const handledAdd = () => {
+    dispatch({ type: "ADD", payload: { num: addelement.current.value } });
+    addelement.current.value = "";
+  };
+  const handledSub = () => {
+    dispatch({ type: "SUB", payload: { num: addelement.current.value } });
+    addelement.current.value = "";
+  };
+  return (<>
     <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
-      <input tpye="text" placeholder="Enter number"ref={addelement}></input>
-   <button type="button" class="btn btn-primary" onClick={handleIncrement}>+1</button>
-   <button type="button" class="btn btn-success" onClick={handledecrement}>-1</button>
-   <button type="button" class="btn btn-info" onClick={handledAdd}>Add</button>
-   <button type="button" class="btn btn-danger" onClick={handledSub}>Substract</button>
-  </div>   
-  )
-}
+      <input
+        tpye="text"
+        placeholder="Enter number"
+        className="input-button"
+        ref={addelement}
+      />
+      </div>
+     
+      <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+      <button type="button" class="btn btn-success" onClick={handledAdd}>
+        Add
+      </button>
+      <button type="button" class="btn btn-danger" onClick={handledSub}>
+        Substract
+      </button>
+      <button type="button" class="btn btn-info" onClick={handleMul}>
+        Multipl
+      </button>
+      <button type="button" class="btn btn-dark" onClick={handleDiv}>
+        Division
+      </button>
+    </div>
+    </>
+  );
+};
 export default Controler;
